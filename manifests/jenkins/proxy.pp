7536c80d1c9f7fa2ece0c3193e9288c9c490005c
faryaz <faryazmd@yahoo.com>
2015-06-18 10:21:38 -0400

    updates

--- manifests/jenkins/proxy.pp
+++ manifests/jenkins/proxy.pp
@@ -1,12 +1,12 @@
 class profiles::jenkins::proxy {
-nginx::resource::upstream { 'jenkins-master.xorlab1.com':
+nginx::resource::upstream { 'jenkins-master.com':
   members => [
     'localhost:8080',
   ],
 }
 
-nginx::resource::vhost { 'jenkins-master.xorlab1.com':
-  proxy => 'http://jenkins-master.xorlab1.com',
+nginx::resource::vhost { 'jenkins-master.com':
+  proxy => 'http://jenkins-master.com',
 #  ssl                  => true,
 #  ssl_cert    => '/etc/puppetlabs/puppet/ssl/certs/jenkins-master.xorlab1.com.pem',
 #  ssl_key     => '/etc/puppetlabs/puppet/ssl/private_keys/jenkins-master.xorlab1.com.pem',
