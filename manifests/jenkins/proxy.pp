9389a57e85de9b50e645318b52937bd106b84fe4
faryaz <faryazmd@yahoo.com>
2015-06-18 08:44:58 -0400

    added ssl

--- manifests/jenkins/proxy.pp
+++ manifests/jenkins/proxy.pp
@@ -8,8 +8,5 @@ nginx::resource::upstream { 'jenkins-master':
 nginx::resource::vhost { 'jenkins-master.com':
   proxy => 'https://jenkins-master',
   ssl                  => true,
-  ssl_cert             => '/var/lib/puppet/ssl/certs/jenkins-master.com.pem',
-  ssl_key              => '/var/lib/puppet/ssl/private_keys/jenkins-master.com.pem',
-  ssl_port             => 443,
 }
 }
