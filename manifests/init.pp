a081fef6047fbd9a731cf67d443645265a4d0b6a
faryaz <faryazmd@yahoo.com>
2015-06-18 05:30:10 -0400

    fixed syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class profiles {
 include profiles::nginx::nginx
-inlcude profiles::psexercise
+include profiles::psexercise
 include profiles::jenkins::jenkins
 include profiles::jenkins::master
 include profiles::jenkins::slave
