e8473b87e51c1939e827b7bd53740d495ca8865f
faryaz <faryazmd@yahoo.com>
2015-06-19 05:05:20 -0400

    added listen port

--- manifests/jenkins/proxy.pp
+++ manifests/jenkins/proxy.pp
@@ -7,7 +7,8 @@ nginx::resource::upstream { 'jenkins-master.xorlab1.com':
 
 nginx::resource::vhost { 'jenkins-master.com':
   proxy => 'https://jenkins-master.xorlab1.com',
-  ssl                  => true,
+  listen_port => 587,
+  ssl         => true,
   ssl_port  => 443,
   ssl_cert    => '/etc/puppetlabs/puppet/ssl/certs/jenkins-master.xorlab1.com.pem',
   ssl_key     => '/etc/puppetlabs/puppet/ssl/private_keys/jenkins-master.xorlab1.com.pem',
