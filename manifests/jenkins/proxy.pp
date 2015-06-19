efb132142b2c694c497eea3c38e57bf7b7ef4a52
faryaz <faryazmd@yahoo.com>
2015-06-19 05:40:51 -0400

    fixes

--- manifests/jenkins/proxy.pp
+++ manifests/jenkins/proxy.pp
@@ -7,10 +7,10 @@ nginx::resource::upstream { 'jenkins-master.xorlab1.com':
 
 nginx::resource::vhost { 'jenkins-master.com':
   proxy => 'http://jenkins-master.xorlab1.com',
-#  ssl         => true,
+  ssl         => true,
 #  ssl_port    => 443,
 #  listen_port => 443,
-#  ssl_cert    => '/etc/puppetlabs/puppet/ssl/certs/jenkins-master.xorlab1.com.pem',
-#  ssl_key     => '/etc/puppetlabs/puppet/ssl/private_keys/jenkins-master.xorlab1.com.pem',
+  ssl_cert    => '/etc/puppetlabs/puppet/ssl/certs/jenkins-master.xorlab1.com.pem',
+  ssl_key     => '/etc/puppetlabs/puppet/ssl/private_keys/jenkins-master.xorlab1.com.pem',
 }
 }
